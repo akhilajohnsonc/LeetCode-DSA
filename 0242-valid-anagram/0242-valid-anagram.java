2260b3441778bd7ed5fd2dcbb34517ffac24c957
Akhila <59962882+akhilajohnsonc@users.noreply.github.com>
2023-03-11 14:27:42 +0530

    Time: 11 ms (32.99%), Space: 43.1 MB (23.43%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        System.out.println(s + "  " + t);
+        if(s.length() != t.length())
+        return false;
+        
+        int[] store = new int[26];
+        for (int i=0;i<s.length();i++){
+            store[s.charAt(i) - 'a']++;
+            store[t.charAt(i) - 'a']--;
+        }
+        for(int n : store){
+            if(n!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
