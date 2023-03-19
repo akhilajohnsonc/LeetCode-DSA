cf851662f7d24e97101a7e12bc3704630d3924ed
Akhila <59962882+akhilajohnsonc@users.noreply.github.com>
2023-03-19 17:09:58 +0530

    Time: 7 ms (80.50%), Space: 45.5 MB (69.86%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -5,18 +5,20 @@ class Solution {
             return result;
         }
         HashMap<String,List<String>> sortedmap = new HashMap<>();
-       for(int i = 0;i<strs.length;i++){
-           char[] ch = strs[i].toCharArray();
+        //traverse through each str in strs.
+        for(int i = 0;i<strs.length;i++){
+            //sort each string
+            char[] ch = strs[i].toCharArray();
             Arrays.sort(ch);
             String sortedString = new String(ch);
-           sortedmap.putIfAbsent(sortedString, new ArrayList<>());
+            //put it in hash map and add str as a list of strings in value part.
+            sortedmap.putIfAbsent(sortedString, new ArrayList<>());
            if(sortedmap.containsKey(sortedString)){    
                sortedmap.get(sortedString).add(strs[i]);   
            }
-          
            //System.out.println(sortedmap.toString()); 
         }
-     result.addAll(sortedmap.values());
+        result.addAll(sortedmap.values());
         return result ;
     }
 }
