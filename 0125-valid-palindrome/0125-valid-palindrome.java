2c544c93619c73561295129cb3df8600b6900917
Akhila <59962882+akhilajohnsonc@users.noreply.github.com>
2023-09-16 11:05:59 +0530

    Time: 3 ms (77.96%), Space: 41.3 MB (99.49%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,12 +1,24 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String cleanSentence = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        StringBuilder sb = new StringBuilder(cleanSentence).reverse();
-        String reversed = sb.toString();
-        System.out.println(sb);
-        System.out.println(cleanSentence);
-        if(cleanSentence.equals(reversed))
+        int i = 0;
+        int j = s.length() - 1;
+        while (i<j){
+            Character start = s.charAt(i);
+            Character end = s.charAt(j);
+            if(!Character.isLetterOrDigit(start)){
+                i++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(end)){
+                j--;
+                continue;   
+            }
+            if(Character.toLowerCase(start) != Character.toLowerCase(end)){
+               return false;
+            }
+            i++;
+            j--;
+        }
         return true;
-        return false;
     }
 }
