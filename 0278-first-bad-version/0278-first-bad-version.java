b7dca310c1eee81553e52ee7422ab103a8337bdc
Akhila <59962882+akhilajohnsonc@users.noreply.github.com>
2023-09-23 23:03:23 +0530

    Time: 24 ms (10.66%), Space: 39.6 MB (16.01%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -7,13 +7,13 @@ public class Solution extends VersionControl {
         //System.out.println(low +" "+ high);
         while (low <= high) {
             int middleposition = low + (high - low) / 2;
-            //System.out.println(middleposition)
+            System.out.println(middleposition);
            if (isBadVersion(middleposition) == false) {
                 low = middleposition + 1;
-                //System.out.println( " Low is " + low);
+                System.out.println( " Low is " + low);
             } else {
                 high = middleposition - 1;
-                //System.out.println( " high is " + high);
+                System.out.println( " high is " + high);
             }
         }
         return high+1;
