b0ec8070794e41c610786e9daf610087af2cd4a0
Akhila <59962882+akhilajohnsonc@users.noreply.github.com>
2023-09-23 22:56:03 +0530

    Time: 25 ms (9.61%), Space: 38.9 MB (86.66%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -1,28 +1,24 @@
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
-
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-    int low = 0;
-        int high = n ;
+        int low = 0;
+        int high = n;
         //System.out.println(low +" "+ high);
-        
-        while(low <= high){
-            int middleposition = low + (high-low)/2;
+        while (low <= high) {
+            int middleposition = low + (high - low) / 2;
             //System.out.println(middleposition);
-             if(isBadVersion(middleposition)==true 
-                && isBadVersion(middleposition-1)==false) {
-                return  middleposition;
-             }  
-           else if (isBadVersion(middleposition) == false ){
- low = middleposition + 1;
-                
+            if (isBadVersion(middleposition) == true &&
+                isBadVersion(middleposition - 1) == false) {
+                return middleposition;
+            } else if (isBadVersion(middleposition) == false) {
+                low = middleposition + 1;
                 //System.out.println( " Low is " + low);
-            }else {       
-               high = middleposition - 1;
+            } else {
+                high = middleposition - 1;
                 //System.out.println( " high is " + high);
             }
         }
-         return -1;
+        return -1;
     }
 }
