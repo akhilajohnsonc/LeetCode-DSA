9809b78c4d7ada48ec55e13931006107c568e0b8
Akhila <59962882+akhilajohnsonc@users.noreply.github.com>
2023-09-24 18:59:11 +0530

    Time: 1 ms (99.72%), Space: 43.7 MB (70.15%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,36 +1,6 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        //HashMap < Character, Integer> map = new HashMap<>();
-        
-        if(ransomNote.length()>magazine.length()) return false;
-        //using hashmap
-        // for(int i = 0; i < magazine.length(); i++){
-        //     char c = magazine.charAt(i);
-        //     if(map.containsKey(c)){
-        //         map.put(c, map.get(c)+1);
-        //     }else{
-        //         map.put(c, 1);
-        //     } 
-        // }
-        //  for(int i = 0; i < ransomNote.length(); i++){
-        //     char c = ransomNote.charAt(i);
-        //     if(map.containsKey(c) == false || map.get(c) == 0){
-        //         return false;
-        //     }
-        //     else{
-        //         map.put(c, map.get(c) - 1);       
-        //     }
-        // }
-        
         int[] alphabet_tracker = new int[26];
-        // for(int i = 0; i < magazine.length(); i++){
-        //      char c = magazine.charAt(i);
-        //     alphabet_tracker[c -'a'] ++;
-        // }
-        // for(int i = 0; i < ransomNote.length(); i++){
-        //      char c = ransomNote.charAt(i);
-        //     alphabet_tracker[c -'a'] --;
-        // }
         for(char c : magazine.toCharArray()){
             ++alphabet_tracker[c -'a'];
         }
@@ -39,10 +9,6 @@ class Solution {
                 return false;
             }
         }
-        // for(int n : alphabet_tracker){
-        //     if(n < 0)
-        //     return false;
-        // }
         return true;
     }
 }
