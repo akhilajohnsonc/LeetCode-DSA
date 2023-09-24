9e24efd67719b25cfb830d63032996984b467107
Akhila <59962882+akhilajohnsonc@users.noreply.github.com>
2023-09-24 18:55:20 +0530

    Time: 3 ms (78.42%), Space: 43.5 MB (89.33%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -30,9 +30,11 @@ class Solution {
         for(int i = 0; i < ransomNote.length(); i++){
              char c = ransomNote.charAt(i);
             alphabet_tracker[c -'a'] --;
-            
-            if(alphabet_tracker[c -'a']<0)
-                return false;
+        }
+        
+        for(int n : alphabet_tracker){
+            if(n < 0)
+            return false;
         }
         return true;
     }
