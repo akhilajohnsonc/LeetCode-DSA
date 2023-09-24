9ec6402c921890a281b0d3206213bd4ef936ff4b
Akhila <59962882+akhilajohnsonc@users.noreply.github.com>
2023-09-24 18:53:09 +0530

    Time: 1 ms (99.72%), Space: 43.8 MB (70.15%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap < Character, Integer> map = new HashMap<>();
+        //HashMap < Character, Integer> map = new HashMap<>();
         
         if(ransomNote.length()>magazine.length()) return false;
         //using hashmap
