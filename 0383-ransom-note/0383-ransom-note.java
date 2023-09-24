f92e85d0d1f8dbf4c5d7783fdbab002aa3d9159d
Akhila <59962882+akhilajohnsonc@users.noreply.github.com>
2023-09-24 19:00:09 +0530

    Time: 1 ms (99.72%), Space: 42.9 MB (99.75%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,14 +1,14 @@
 class Solution {
-    public boolean canConstruct(String ransomNote, String magazine) {
-        int[] alphabet_tracker = new int[26];
-        for(char c : magazine.toCharArray()){
-            ++alphabet_tracker[c -'a'];
-        }
-        for(char c : ransomNote.toCharArray()){
-            if(--alphabet_tracker[c -'a']<0){
-                return false;
-            }
-        }
-        return true;
+  public boolean canConstruct(String ransomNote, String magazine) {
+    int[] alphabet_tracker = new int[26];
+    for (char c: magazine.toCharArray()) {
+      ++alphabet_tracker[c - 'a'];
     }
+    for (char c: ransomNote.toCharArray()) {
+      if (--alphabet_tracker[c - 'a'] < 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
