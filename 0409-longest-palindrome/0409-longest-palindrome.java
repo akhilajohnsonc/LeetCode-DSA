46bf4ce2c205e553abcf0f2de9f325c6cf2f3d46
Akhila <59962882+akhilajohnsonc@users.noreply.github.com>
2023-09-28 11:39:45 +0530

    Time: 1 ms (100.00%), Space: 41.2 MB (23.22%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,31 +1,31 @@
 class Solution {
-    public int longestPalindrome(String s) {
-        //using set runtime 5s, beats 65%
-        // Set<Character> charSet = new HashSet<>();
-        // for(char c : s.toCharArray()){
-        //     if (charSet.contains(c)){
-        //         charSet.remove(c);
-        //     }else {
-        //        charSet.add(c); 
-        //     }
-        // }
-        // int len = s.length();
-        // int set_size = charSet.size();
-        // int result = (set_size == 0 ) ? len : len - set_size + 1;
-        // return result;
-        
-        //using Array runtime: 1ms beats 100%
-        int chars[] = new int[128];
-        for(char c : s.toCharArray()){
-            chars[c]++;
-        }
-        int result = 0;
-        for( int n : chars){
-            result += n/2 * 2 ;  // 6/2 *2 = 6, 7/2*2 = 6 ; 
-            if(result %2 ==0 && n %2 ==1){
-                result ++;
-            }
-        }
-        return result;
+  public int longestPalindrome(String s) {
+    //using set runtime 5s, beats 65%
+    // Set<Character> charSet = new HashSet<>();
+    // for(char c : s.toCharArray()){
+    //     if (charSet.contains(c)){
+    //         charSet.remove(c);
+    //     }else {
+    //        charSet.add(c); 
+    //     }
+    // }
+    // int len = s.length();
+    // int set_size = charSet.size();
+    // int result = (set_size == 0 ) ? len : len - set_size + 1;
+    // return result;
+
+    //using Array runtime: 1ms beats 100%
+    int chars[] = new int[128];
+    for (char c: s.toCharArray()) {
+      chars[c]++;
     }
+    int result = 0;
+    for (int n: chars) {
+      result += n / 2 * 2; // 6/2 *2 = 6, 7/2*2 = 6 ; 
+      if (result % 2 == 0 && n % 2 == 1) {
+        result++;
+      }
+    }
+    return result;
+  }
 }
