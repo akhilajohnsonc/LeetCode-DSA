23afbdbee01391429ffb1b3f1b6625f88f0b382b
Akhila <59962882+akhilajohnsonc@users.noreply.github.com>
2023-09-28 11:37:50 +0530

    Time: 1 ms (100.00%), Space: 40.2 MB (98.91%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,16 +1,31 @@
 class Solution {
     public int longestPalindrome(String s) {
-        Set<Character> charSet = new HashSet<>();
+        //using set runtime 5s, beats 65%
+        // Set<Character> charSet = new HashSet<>();
+        // for(char c : s.toCharArray()){
+        //     if (charSet.contains(c)){
+        //         charSet.remove(c);
+        //     }else {
+        //        charSet.add(c); 
+        //     }
+        // }
+        // int len = s.length();
+        // int set_size = charSet.size();
+        // int result = (set_size == 0 ) ? len : len - set_size + 1;
+        // return result;
+        
+        //using Array
+        int chars[] = new int[128];
         for(char c : s.toCharArray()){
-            if (charSet.contains(c)){
-                charSet.remove(c);
-            }else {
-               charSet.add(c); 
+            chars[c]++;
+        }
+        int result = 0;
+        for( int n : chars){
+            result += n/2 * 2 ;  // 6/2 *2 = 6, 7/2*2 = 6 ; 
+            if(result %2 ==0 && n %2 ==1){
+                result ++;
             }
         }
-        int len = s.length();
-        int set_size = charSet.size();
-        int result = (set_size == 0 ) ? len : len - set_size + 1;
         return result;
     }
 }
