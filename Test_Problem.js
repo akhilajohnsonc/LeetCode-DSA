6836c0f6a60b5e8c32b0febf1aeba4d7ed28fac3
Akhila <59962882+akhilajohnsonc@users.noreply.github.com>
2025-03-30 18:42:12 +0530

    Solved: Test Problem (Medium)

---
+++ Test_Problem.js
@@ -0,0 +1,10 @@
+/*
+  * Problem: Test Problem
+  * Difficulty: Medium
+  * URL: https://leetcode.com/problems/partition-labels/submissions/1591074607/?envType=daily-question&envId=2025-03-30
+  * Date: 2025-03-30
+  */
+  
+  function testFunction() {
+  return "This is a test";
+}
